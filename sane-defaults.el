ac874066e4224694c9d9bfdf1ebc20bf7481e1bd
Ben Hayden <ben@britecore.com>
2014-03-21 13:15:29 -0500

    Add 2 finger scroll via @iwschris

--- sane-defaults.el
+++ sane-defaults.el
@@ -1,8 +1,18 @@
 ;; Allow pasting selection outside of Emacs
 (setq x-select-enable-clipboard t)
 
-;; Terminal Mouse Control
-(xterm-mouse-mode 1)
+;; Terminal Mouse Control & two-finger scrolling
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] '(lambda ()
+                              (interactive)
+                              (scroll-down 1)))
+  (global-set-key [mouse-5] '(lambda ()
+                              (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t))
 
 ;; Auto refresh buffers
 (global-auto-revert-mode 1)
