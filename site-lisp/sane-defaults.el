491a37beff955f183ce7b4b6376ae9516568e02f
Ben Hayden <ben@britecore.com>
2014-04-21 09:52:27 -0500

    Remove ido at point

--- site-lisp/sane-defaults.el
+++ site-lisp/sane-defaults.el
@@ -148,6 +148,6 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
-(setq ido-use-filename-at-point 'guess)
+(setq ido-use-filename-at-point nil)
 
 (provide 'sane-defaults)
