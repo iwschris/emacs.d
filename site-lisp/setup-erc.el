948c8b6565ce3208d2111ef14b25fabeefdb13c9
Ben Hayden <ben@britecore.com>
2014-04-24 23:34:22 -0500

    Some ERC colors.

--- site-lisp/setup-erc.el
+++ site-lisp/setup-erc.el
@@ -4,4 +4,8 @@
 ;; Ignore some track stuff
 (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT"))
 
+;; Set up custom colors
+(set-face-attribute 'erc-bold-face nil :foreground (find-color 'yellow))
+(set-face-attribute 'erc-button nil :inherit 'link)
+
 (provide 'setup-erc)
