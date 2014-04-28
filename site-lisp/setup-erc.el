e33531187b7e5372684e66b152136a9d9e8653f5
Ben Hayden <ben@britecore.com>
2014-04-28 15:29:50 -0500

    More ERC

--- site-lisp/setup-erc.el
+++ site-lisp/setup-erc.el
@@ -4,6 +4,9 @@
 ;; Ignore some track stuff
 (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT"))
 
+;; Remove control characters that try to make the client beep
+(setq erc-interpret-controls-p 'remove)
+
 ;; Set up custom colors
 (set-face-attribute 'erc-bold-face nil :foreground (find-color 'yellow))
 (set-face-attribute 'erc-button nil :inherit 'link)
