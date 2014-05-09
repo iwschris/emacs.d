36276645fe24fbd58f27941c781b2de9c60e3f01
Ben Hayden <ben@britecore.com>
2014-05-09 14:51:11 -0500

    Mac stuff.

--- site-lisp/mac.el
+++ site-lisp/mac.el
@@ -12,4 +12,14 @@
 ;; Set multiterm to brew installed zsh
 (setq multi-term-program "/usr/local/bin/zsh")
 
+;; Set up command + c, v for Mac style copy & paste
+(global-set-key (kbd "M-v") 'yank)
+(global-set-key (kbd "s-v") 'scroll-down-command)
+(global-set-key (kbd "M-c") 'ns-copy-including-secondary)
+(global-set-key (kbd "s-c") 'capitalize-word)
+
+;; Flip other frame
+(global-set-key (kbd "M-`") 'other-frame)
+(global-set-key (kbd "s-`") 'tmm-menubar)
+
 (provide 'mac)
