8418322e7febd8639c9689fd59935b5ec6913bf3
Ben Hayden <ben@britecore.com>
2014-05-22 10:49:51 -0500

    Adding number highlighting.

--- site-lisp/mode-hooks.el
+++ site-lisp/mode-hooks.el
@@ -3,12 +3,40 @@
 ;; Run Go fmt on Go files before writing
 (add-hook 'before-save-hook 'gofmt-before-save)
 
-;; XXX: Highlights some important keywords - the ':' is important
 (add-hook 'prog-mode-hook
           (lambda()
-            (font-lock-add-keywords nil
-                                    '(("\\<\\(FIXME\\|TODO\\|XXX+\\|BUG\\):"
-                                       1 font-lock-warning-face prepend)))
+            (font-lock-add-keywords nil '(
+                                          ;; XXX: Highlight important words in comments - ':' is necessary
+                                          ("\\<\\(FIXME\\|TODO\\|XXX+\\|BUG\\):" 1 font-lock-warning-face prepend)
+
+                                          ;; Valid hex number (will highlight invalid suffix though)
+                                          ("\\b0x[[:xdigit:]]+[uUlL]*\\b" . font-lock-string-face)
+
+                                          ;; Invalid hex number
+                                          ("\\b0x\\(\\w\\|\\.\\)+\\b" . font-lock-warning-face)
+
+                                          ;; Valid floating point number.
+                                          ("\\(\\b[0-9]+\\|\\)\\(\\.\\)\\([0-9]+\\(e[-]?[0-9]+\\)?\\([lL]?\\|[dD]?[fF]?\\)\\)\\b" (1 font-lock-string-face) (3 font-lock-string-face))
+
+                                          ;; FIXME: Invalid floating point number.  Must be before valid decimal.
+                                          ;; ("\\b[0-9].*?\\..+?\\b" . font-lock-warning-face)
+
+                                          ;; Valid decimal number.  Must be before octal regexes otherwise 0 and 0l
+                                          ;; will be highlighted as errors.  Will highlight invalid suffix though.
+                                          ("\\b\\(\\(0\\|[1-9][0-9]*\\)[uUlL]*\\)\\b" 1 font-lock-string-face)
+
+                                          ;; Valid octal number
+                                          ("\\b0[0-7]+[uUlL]*\\b" . font-lock-string-face)
+
+                                          ;; Floating point number with no digits after the period.  This must be
+                                          ;; after the invalid numbers, otherwise it will "steal" some invalid
+                                          ;; numbers and highlight them as valid.
+                                          ("\\b\\([0-9]+\\)\\." (1 font-lock-string-face))
+
+                                          ;; Invalid number.  Must be last so it only highlights anything not
+                                          ;; matched above.
+                                          ("\\b[0-9]\\(\\w\\|\\.\\)+?\\b" . font-lock-warning-face)
+                                          ))
             (wrap-region-mode)
             (yas-minor-mode)
             (if (fboundp 'superword-mode)
